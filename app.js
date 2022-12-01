3a984e44512e47dace79984e40d2c01a20ca102b
Matheus Dias <matheusdias.front96@gmail.com>
2022-12-01 16:41:34 -0300

    'Refatoração-de-codigo'

--- app.js
+++ app.js
@@ -3,41 +3,50 @@ const resultWrapper = document.querySelector(".result-wrapper");
 const result = document.querySelector(".result");
 const button = document.querySelector(".btn");
 const resultQuiz = document.querySelector("span");
+
 const correctAnswers = ["A", "C", "C", "A", "B", "A", "B", "C", "B", "C"];
 
-const showResult = (event) => {
-  event.preventDefault();
+let score = 0;
 
-  let score = 0;
-  let counter = 0;
+const getUserAnswers = () => {
+  const userAnswer = [];
 
-  const userAnswers = [
-    form.inputQuestion1.value,
-    form.inputQuestion2.value,
-    form.inputQuestion3.value,
-    form.inputQuestion4.value,
-    form.inputQuestion5.value,
-    form.inputQuestion6.value,
-    form.inputQuestion7.value,
-    form.inputQuestion8.value,
-    form.inputQuestion9.value,
-    form.inputQuestion10.value,
-  ];
-  userAnswers.forEach((userAnswer, index) => {
-    if (userAnswer === correctAnswers[index]) {
+  correctAnswers.forEach((_, index) => {
+    const userAnswers = form[`inputQuestion${index + 1}`].value;
+    userAnswer.push(userAnswers);
+  });
+  return userAnswer;
+};
+
+const calculateUserScore = (userAnswers) => {
+  userAnswers.forEach((userAnswers, index) => {
+    const isUserAnswerCorrect = userAnswers === correctAnswers[index];
+    if (isUserAnswerCorrect) {
       score += 10;
+      console.log(score);
     }
   });
+};
+const finalScore = () => {
   scrollTo(0, 0);
   resultWrapper.style.display = "block";
+};
 
+const successOrDanger = (score) => {
+  if (score >= 50) {
+    result.setAttribute("class", "bg-success");
+    return;
+  }
+  result.setAttribute("class", "bg-danger");
+};
+
+const showResust = () => {
+  let counter = 0;
   const timer = setInterval(() => {
     if (counter === score) {
       clearInterval(timer);
     }
-    resultQuiz.textContent = `${counter}%`;
-
-    counter++;
+    resultQuiz.textContent = `${counter++}%`;
   }, 10);
   if (score >= 50) {
     result.setAttribute("class", "bg-success");
@@ -46,7 +55,7 @@ const showResult = (event) => {
   result.setAttribute("class", "bg-danger");
 };
 
-const existResult = (event) => {
+const closeTheResult = (event) => {
   const classOfClicked = event.target.classList[0];
   const classNames = ["result-close", "result-button", "result-wrapper"];
   const shoulResult = classNames.some(
@@ -55,7 +64,15 @@ const existResult = (event) => {
   if (shoulResult) {
     resultWrapper.style.display = "none";
   }
+  score = 0;
 };
 
-form.addEventListener("submit", showResult);
-result.addEventListener("click", existResult);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userAnswers = getUserAnswers();
+  calculateUserScore(userAnswers);
+  showResust();
+  finalScore();
+});
+
+result.addEventListener("click", closeTheResult);
